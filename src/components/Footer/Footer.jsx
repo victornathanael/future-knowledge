aca6519d6ebc0a302edc73ad2fee66788ecb0ae0
Victor Nathanael <victornathanael15@gmail.com>
2023-09-22 23:42:25 -0300

    feat: add footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,46 @@
+import './Footer.css';
+import fkLogoFooter from '../../assets/future-knowledge-logo-footer.png';
+import linkedinIcon from '../../assets/linkedin-icon.svg';
+import youtubeIcon from '../../assets/youtube-icon.svg';
+import instagramIcon from '../../assets/instagram-icon.svg';
+
+export default function Footer() {
+    return (
+        <footer>
+            <div id='footer-container'>
+                <div id='info-site'>
+                    <img id='future-knowledge-logo-footer' src={fkLogoFooter} alt='' />
+                    <div id='info-site-links'>
+                        <p>Termos de Uso</p>
+                        <p>Política de Privacidade</p>
+                        <p>Cookies</p>
+                    </div>
+                </div>
+                <div id='future-experience'>
+                    <h4>FUTURE EXPERIENCE</h4>
+                    <ul>
+                        <li>Home</li>
+                        <li>Mural de avisos</li>
+                    </ul>
+                </div>
+                <div id='student-dashboard'>
+                    <h4>PAINEL DO ALUNO</h4>
+                    <ul>
+                        <li>Aulas</li>
+                        <li>Desafios</li>
+                        <li>Ranking</li>
+                        <li>Feedbacks</li>
+                    </ul>
+                </div>
+                <div id='social-media'>
+                    <h4>SOCIAL MEDIA</h4>
+                    <div id='icons'>
+                        <img src={linkedinIcon} alt='icon of linkedin' />
+                        <img src={youtubeIcon} alt='icon of youtube' />
+                        <img src={instagramIcon} alt='icon of instagram' />
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
