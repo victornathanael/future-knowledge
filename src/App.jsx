3840e838dca5e82ff887afb27c3739cfcfe60b8a
Victor Nathanael <victornathanael15@gmail.com>
2023-09-23 10:41:26 -0300

    feat: add home page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
 
 function App() {
-    return <div></div>;
+    return (
+        <div id='App'>
+            <Header />
+            <Home />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
