41888c58ad74df37e6527546978572cc53378671
Victor Nathanael <victornathanael15@gmail.com>
2023-09-22 21:21:48 -0300

    refactor: remove width property on style of header and rename the logo image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,22 @@
 import './Header.css';
-import futureKnowledgeLogo from '../../assets/future-knowledge-logo.png';
+import fkLogoHeader from '../../assets/future-knowledge-logo.png';
 import profilePhoto from '../../assets/profile-photo.svg';
 
 export default function Header() {
     return (
-        <header>
-            <div id='header-container'>
-                <img
-                    id='future-knowledge-logo'
-                    src={futureKnowledgeLogo}
-                    alt='image written future knowledge'
-                />
-                <div id='links-container'>
+        <header id='header-container'>
+            <img
+                id='future-knowledge-logo'
+                src={fkLogoHeader}
+                alt='image written future knowledge'
+            />
+            <div id='items-header'>
+                <div id='links'>
                     <p>AULAS</p>
                     <p>DESAFIO</p>
                     <p>RANKING</p>
                     <p>FEEDBACKS</p>
                 </div>
-
                 <img id='profile-photo' src={profilePhoto} alt='profile image' />
             </div>
         </header>
