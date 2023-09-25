078e9ca4034a1d5661e69aefc942a91e9943d1ce
Victor Nathanael <victornathanael15@gmail.com>
2023-09-25 16:33:16 -0300

    feat: add image to list item user component

--- src/components/ListItemUser/ListItemUser.jsx
+++ src/components/ListItemUser/ListItemUser.jsx
@@ -4,7 +4,7 @@ export default function ListItemUser({ rank, image, name, points, stack }) {
     return (
         <div className='list-item-user'>
             {rank && <p className='list-item-user__rank'>{rank}º</p>}
-            <img src={image} alt='person face image' />
+            <img className='list-item-user__image' src={image} alt='person face image' />
             <p className='list-item-user__name'>{name}</p>
             {points && <p className='list-item-user__points'>{points}pts</p>}
             {stack && <p className='list-item-user__stack'>[{stack}]</p>}
