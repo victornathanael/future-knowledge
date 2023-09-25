404805a597f154f5058c69fba6405b9a72a15383
Victor Nathanael <victornathanael15@gmail.com>
2023-09-25 16:34:57 -0300

    refactor: modify style for background components

--- src/components/BackgroundRanking/BackgroundRanking.jsx
+++ src/components/BackgroundRanking/BackgroundRanking.jsx
@@ -8,11 +8,11 @@ import wavyLineYellow from '../../assets/wavy-line-yellow.svg';
 export default function BackgroundRanking() {
     return (
         <>
-            <img id='wavy-line-yellow-top-ranking' src={wavyLineYellow} alt='purple wavy line' />
+            <img id='wavy-line-yellow-ranking-top' src={wavyLineYellow} alt='purple wavy line' />
             <img id='yellow-circle-ranking' src={yellowCircle} alt='yellow circle' />
             <img id='white-circle-ranking' src={whiteCircle} alt='white circle' />
             <img id='triangle-purple-ranking' src={trianglePurple} alt='triangle purple' />
-            <img id='wavy-line-yellow-ranking' src={wavyLineYellow} alt='wavy line yellow' />
+            <img id='wavy-line-yellow-ranking-bottom' src={wavyLineYellow} alt='wavy line yellow' />
         </>
     );
 }
